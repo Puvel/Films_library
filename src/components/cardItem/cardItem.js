e45ffd9cc80999b2ca05655a752cd100e8167f4d
Your Name <you@example.com>
2020-04-21 22:37:06 +0300

    init card_description_branch

--- src/components/cardItem/cardItem.js
+++ src/components/cardItem/cardItem.js
@@ -1,31 +1,50 @@
 import './cardItem.css';
-import api from '../../services/api';
+import services from '../../services/services';
 import filmCardTemplate from '../../templates/filmCardTemplate.hbs';
 
+const refs = {
+  galleryList: document.querySelector('.js-gallery_list'),
+};
+
+refs.galleryList.addEventListener('click', renderCardFilm);
+
+function renderCardFilm(e) {
+  if (e.target.nodeName === 'BUTTON') {
+    const button = e.target;
+    const li = button.closest('.gallery-list__item');
+    const id = li.dataset.id;
+
+    createCardFilm(id);
+  }
+}
+
 const container = document.querySelector('.main_section');
-api
-  .fetchOneFilm(443791)
-  .then(card => {
-    let newGenres = card.genres.map((genre, idx) => {
-      if (card.genres.length - 1 !== idx) {
-        return {
-          ...genre,
-          name: formatString(genre.name),
-        };
-      } else {
-        return { ...genre };
-      }
-    });
 
-    card.genres = newGenres;
-    insertCardItems(card);
-  })
+function createCardFilm(id) {
+  services
+    .fetchMovieCardApi(id)
+    .then(card => {
+      let newGenres = card.genres.map((genre, idx) => {
+        if (card.genres.length - 1 !== idx) {
+          return {
+            ...genre,
+            name: formatString(genre.name),
+          };
+        } else {
+          return { ...genre };
+        }
+      });
 
-  .catch(err => console.log(err));
+      card.genres = newGenres;
+      insertCardItems(card);
+    })
 
-function insertCardItems(film) {
-  const markup = filmCardTemplate(film);
-  container.insertAdjacentHTML('beforeend', markup);
+    .catch(err => console.log(err));
+
+  function insertCardItems(film) {
+    const markup = filmCardTemplate(film);
+    container.innerHTML = markup;
+  }
 }
 
 function formatString(string) {
