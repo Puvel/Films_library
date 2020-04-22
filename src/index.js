fb768c88eca08bec926d94c787f1aec0ebdbee6d
Olia1011 <olgamelnyk10111989@gmail.com>
2020-04-22 16:01:30 +0300

    add inf

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './components/Navigation/navigation';
 import './components/pagination/pagination.js';
 import './components/cardItem/cardItem.css';
 import './components/cardItem/cardItem';
-import './components/searchForm/searchForm';
 import './components/mainFilmsList/mainFilmsList.js';
 import './components/searchForm/searchForm';
+// import './components/searchBtn/searchBtn'
+
